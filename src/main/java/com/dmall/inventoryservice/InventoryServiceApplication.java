9225e3d4efb8e4e3e6673d29fb9ffc3d1e1cc57c
Jamin Huang <jamin.huang@cargosmart.com>
2019-06-27 15:09:17 +0800

    add eureka config

--- src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
@@ -2,10 +2,12 @@ package com.dmall.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
